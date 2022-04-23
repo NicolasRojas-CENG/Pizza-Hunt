f41446c8fce78cc15831783750e5664b92bda29d
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-04-22 22:14:02 -0400

    Added validation to the documents.

--- models/Comment.js
+++ models/Comment.js
@@ -9,17 +9,21 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
         }
-    },{
+    },
+    {
         toJSON: {
             getters: true
         }
@@ -28,10 +32,14 @@ const ReplySchema = new Schema(
 
 const CommentSchema = new Schema({
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createdAt: {
             type: Date,
